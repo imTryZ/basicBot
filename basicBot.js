5a315f15b6c2f96d7772503282cbc3080418166d
imTryZ <imTryZ@outlook.com>
2015-05-03 19:24:45 -0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2656,6 +2656,19 @@
                 }
             },
             
+             hyubbCommand: {
+                command: 'hyubb',
+                rank: 'user',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        API.sendChat(basicBot.chat.hubb)
+                    }
+                }
+            },
+            
             purchaseCommand: {
                 command: ['assinar'],
                 rank: 'user',
