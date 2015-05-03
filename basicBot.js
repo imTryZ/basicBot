6b22465ca2d0e3a365e964cd2ca70ad6dc022bdc
imTryZ <imTryZ@outlook.com>
2015-05-03 11:18:50 -0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2642,7 +2642,20 @@
                     }
                 }
             },
-
+            
+            FusyonCommand: {
+                command: 'Fusyon',
+                rank: 'user',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        API.sendChat(basicBot.chat.Fusyon)
+                    }
+                }
+            },
+            
             purchaseCommand: {
                 command: ['assinar'],
                 rank: 'user',
